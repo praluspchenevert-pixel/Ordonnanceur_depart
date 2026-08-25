600107368175421871b19b265e37c886e7e124d0
Pierre Pralus <vous@exemple.com>
2026-08-25 13:59:38 +0200

    Changement du titre

--- pigoptim_app.py
+++ pigoptim_app.py
@@ -256,7 +256,7 @@ run_clicked = st.sidebar.button("Lancer la simulation", type="primary")
 # ------------------------------------------------------------
 # EXECUTION DU MODELE
 # ------------------------------------------------------------
-st.title("Ordonnanceur — modele porc charcutier ça te plait mathieu ?")
+st.title("Ordonnanceur — modele porc charcutier ça te plait mat ?")
 
 if run_clicked:
     if weights is None or len(weights) < 2:
